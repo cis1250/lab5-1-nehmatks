00185144f6dbc2f703582351da5cf5ebfff2da35
Nehmat Sohal <nehmat@uoguelph.ca>
2025-10-22 12:13:50 -0400

    Update word_frequency.py

--- word_frequency.py
+++ word_frequency.py
@@ -24,3 +24,49 @@ def is_sentence(text):
         return False
 
     return True
+
+
+# get a sentence from user and check if it meets requirements 
+def get_sentence(): 
+    while True: 
+        try: 
+            sentence = str(input("Enter a sentence: "))
+            if is_sentence(sentence) is True: 
+                return (sentence)
+            else: 
+                print("Please enter a valid sentence.")
+        except: 
+            printf("Please enter a valid sentence.")
+
+# calculates the frequencies 
+def calculate_frequencies(sentence):
+    sentence = sentence.lower() 
+    accepted_char = "abcdefghijklmnopqrstuvwxyz "
+
+    for character in sentence:
+        if character not in accepted_char:
+            sentence =  sentence.replace(character, "")
+
+    # Track the number of times each word appears 
+    sep_words = sentence.split(" ")
+
+    words_list = []
+    words_count = []
+    for word in sep_words: 
+        if word not in words_list: 
+            words_list.append(word)
+            words_count.append(1)
+        else: 
+            count_index = words_list.index(word)
+            words_count[count_index] += 1
+    return (words_list, words_count)
+# output frequencies 
+def print_frequencies(words, frequencies):
+    for i in range(len(words)):
+        print(f"{words[i]}: {frequencies[i]}")
+
+
+# Run the functions 
+sentence = get_sentence()
+words, frequencies = calculate_frequencies(sentence)
+print_frequencies(words, frequencies)
